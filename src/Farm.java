62e034311cdb5738719778fc901c5d41b7a5950d
dawe129 <dpivonka1@gmail.com>
2024-03-11 08:37:45 +0100

    added method for flower

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,16 @@
-public class Farm {
+import java.util.ArrayList;
 
+public class Farm extends Flower {
 
+    ArrayList<Flower> flowers;
 
+    public void vyrostlaRostlina(Flower flower) {
+        for (Flower flower1 : flowers) {
+            if (flower.voda = (true)) {
+                setChanceOfGrowth(60);
+            } else {
+                System.out.println("Nepodarilo se rostlionu zalit");
+            }
+        }
+    }
 }
--- src/Flower.java
+++ src/Flower.java
@@ -5,22 +5,38 @@ public abstract class Flower {
     protected String name;
     protected double price;
     protected double neededArea;
-    protected
+    protected boolean voda;
+    protected int druhy = 5;
     protected double chanceOfGrowth = 50;
 
     ArrayList<Flower> kytky = new ArrayList<>();
 
-    public void vyrostlaRostlina(Flower flower){
-        if(Flower.get)
-    }
-    @Override
-    public String toString() {
-        return "Flower{" +
-                "name='" + name + '\'' +
-                ", price=" + price +
-                ", neededArea=" + neededArea +
-                ", chanceOfGrowth=" + chanceOfGrowth +
-                '}';
+    public boolean isVoda() {
+        return voda;
     }
 
+    public double getChanceOfGrowth() {
+        return chanceOfGrowth;
+    }
+
+    public void setChanceOfGrowth(double chanceOfGrowth) {
+        this.chanceOfGrowth = chanceOfGrowth;
+    }
+
+    public void setVoda(boolean voda) {
+        this.voda = voda;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Flower{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", price=").append(price);
+        sb.append(", neededArea=").append(neededArea);
+        sb.append(", voda=").append(voda);
+        sb.append(", chanceOfGrowth=").append(chanceOfGrowth);
+        sb.append(", kytky=").append(kytky);
+        sb.append('}');
+        return sb.toString();
+    }
 }
