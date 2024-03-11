f09f1fc5f9bee0961e8aecfc1c5c137dc5cf6f3c
dawe129 <dpivonka1@gmail.com>
2024-03-11 08:57:55 +0100

    Vytvoreni kytky

--- src/Kytka.java
+++ src/Kytka.java
@@ -1,7 +1,20 @@
-public class Kytka extends Flower{
+public class Kytka extends Flower {
 
     public Kytka(String name, double price, double neededArea, boolean voda, double chanceOfGrowth) {
         super(name, price, neededArea, voda, chanceOfGrowth);
     }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Kytka{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", price=").append(price);
+        sb.append(", neededArea=").append(neededArea);
+        sb.append(", voda=").append(voda);
+        sb.append(", chanceOfGrowth=").append(chanceOfGrowth);
+        sb.append(", kytky=").append(kytky);
+        sb.append('}');
+        return sb.toString();
+    }
 }
-}
+
